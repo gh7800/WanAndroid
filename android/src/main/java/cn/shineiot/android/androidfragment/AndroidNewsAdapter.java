4b2c7a9f00de11024922145f6ea43f52c5ef9b3c
wangs <wangshuai_wy@126.com>
2019-12-03 23:42:02 +0800

    '过滤title里的HTML标签'

--- android/src/main/java/cn/shineiot/android/androidfragment/AndroidNewsAdapter.java
+++ android/src/main/java/cn/shineiot/android/androidfragment/AndroidNewsAdapter.java
@@ -8,7 +8,6 @@ import com.chad.library.adapter.base.BaseViewHolder;
 
 import cn.shineiot.android.R;
 import cn.shineiot.android.bean.AndroidNews;
-import cn.shineiot.base.utils.LogUtil;
 
 
 /**
@@ -22,7 +21,8 @@ public class AndroidNewsAdapter extends BaseQuickAdapter<AndroidNews.News, BaseV
 
 	@Override
 	protected void convert(BaseViewHolder helper, AndroidNews.News item) {
-		helper.setText(R.id.new_chapterName, item.getChapterName()).setText(R.id.new_title, item.getTitle()).setText(R.id.new_niceDate, item.getNiceDate()).setText(R.id.new_author, TextUtils.isEmpty(item.getShareUser()) ? item.getAuthor() : item.getShareUser()).addOnClickListener(R.id.new_checkBox);
+		String title = delHtmlTags(item.getTitle());
+		helper.setText(R.id.new_chapterName, item.getChapterName()).setText(R.id.new_title, title).setText(R.id.new_niceDate, item.getNiceDate()).setText(R.id.new_author, TextUtils.isEmpty(item.getShareUser()) ? item.getAuthor() : item.getShareUser()).addOnClickListener(R.id.new_checkBox);
 
 		CheckBox checkBox = helper.getView(R.id.new_checkBox);
 		if (item.isCollect()) {
@@ -31,5 +31,24 @@ public class AndroidNewsAdapter extends BaseQuickAdapter<AndroidNews.News, BaseV
 			checkBox.setChecked(false);
 		}
 	}
+	public static String delHtmlTags(String htmlStr) {
+		//定义script的正则表达式，去除js可以防止注入
+		String scriptRegex="<script[^>]*?>[\\s\\S]*?<\\/script>";
+		//定义style的正则表达式，去除style样式，防止css代码过多时只截取到css样式代码
+		String styleRegex="<style[^>]*?>[\\s\\S]*?<\\/style>";
+		//定义HTML标签的正则表达式，去除标签，只提取文字内容
+		String htmlRegex="<[^>]+>";
+		//定义空格,回车,换行符,制表符
+		String spaceRegex = "\\s*|\t|\r|\n";
 
+		// 过滤script标签
+		htmlStr = htmlStr.replaceAll(scriptRegex, "");
+		// 过滤style标签
+		htmlStr = htmlStr.replaceAll(styleRegex, "");
+		// 过滤html标签
+		htmlStr = htmlStr.replaceAll(htmlRegex, "");
+		// 过滤空格等
+		htmlStr = htmlStr.replaceAll(spaceRegex, "");
+		return htmlStr.trim(); // 返回文本字符串
+	}
 }
